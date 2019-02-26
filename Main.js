31f069795940507276eb10c2203e9014ddf4b2d8
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-02-26 08:40:52 +0000

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
 const alphabet = [...Array(26).keys()].map(n => String.fromCharCode(n + 97)).join("");
 
 function product(...nums) {
-    var answer = 1;
-    for (var n of nums) {
+    let answer = 1;
+    for (let n of nums) {
         answer *= n;
     }
     return answer;
@@ -10,18 +10,18 @@ function product(...nums) {
 
 function * combinations (...options) {
     console.log(options);
-    var combo = Array(options.length).fill(0);
-    var repetitions = [...Array(options.length).keys()]
+    let combo = Array(options.length).fill(0);
+    let repetitions = [...Array(options.length).keys()]
     repetitions = repetitions.map(i => product(...options.slice(i).map(arr => arr.length))).reverse();
     console.log(repetitions);
     to = product(...options.map(arr => arr.length))
     console.log("Up to:", to);
     for (var i = 0; i < to; i++){
-        for (var pos = 0; pos < options.length; pos++) {
+        for (let pos = 0; pos < options.length; pos++) {
             combo[pos] = (Math.floor(i / Math.floor(repetitions[pos] / options[pos].length))) % options[pos].length
         }
         values = combo;
-        for (var n = 0; n < values.length; n++) {
+        for (let n = 0; n < values.length; n++) {
             values[n] = options[n][values[n]];
         }
         if (i % 1000 == 0) {
@@ -34,7 +34,7 @@ function * combinations (...options) {
 //console.log(Array.from(combinations(alphabet.toUpperCase(), alphabet, alphabet, alphabet, "123")));
 
 
-for (var word of combinations(alphabet.toUpperCase(), alphabet, alphabet, "0123456789")) {
+for (let word of combinations(alphabet.toUpperCase(), alphabet, alphabet, "0123456789")) {
     document.getElementsByTagName('input')[3].value = word
     document.getElementsByTagName('input')[4].click()
     if (document.URL != "https://moodle.cedarsupper.org.uk/moodle/mod/quiz/startattempt.php") {
